61b7eb84f6cf1201b049a02f3d99f0a51e9a437d
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-07-25 16:26:16 +0200

    Real first commit

--- main.tf
+++ main.tf
@@ -2,3 +2,58 @@
 # Resources
 # ------------------------------------------------------------------------------
 
+resource "aws_ecr_repository" "ecr-repo" {
+  name = "${var.name_prefix}"
+}
+
+resource "aws_ecr_repository_policy" "ecr-policy" {
+  repository = "${aws_ecr_repository.ecr-repo.id}"
+  policy     = "${data.aws_iam_policy_document.ecr-policy-doc.json}"
+}
+
+data "aws_iam_policy_document" "ecr-policy-doc" {
+  "statement" {
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "${formatlist("arn:aws:iam::%s:root", var.trusted_accounts)}",
+      ]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+    ]
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "keep-last-N" {
+  repository = "${aws_ecr_repository.ecr-repo.id}"
+
+  policy = <<EOF
+  {
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last N images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": "${var.lifecycle_count_number}"
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+  }
+EOF
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
 # ------------------------------------------------------------------------------
 # Output
 # ------------------------------------------------------------------------------
+output "repository_arn" {
+  description = "Full ARN of the repository."
+  value       = "${aws_ecr_repository.ecr-repo.arn}"
+}
 
+output "repository_url" {
+  description = "The URL of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName )"
+  value       = "${aws_ecr_repository.ecr-repo.repository_url}"
+}
